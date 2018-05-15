a0e6dedeac6b73e9b84e96795fd243dc6430cc26
ErikBoesen <me@erikboesen.com>
2018-05-15 15:29:44 -0400

    Add missing semicolon

--- quiztools.js
+++ quiztools.js
@@ -3,7 +3,7 @@ if (done_button != null) {
     console.log('SummerFast: running at ' + location.href);
     unlock_button = document.createElement('button');
     unlock_button.type = 'button';
-    unlock_button.id = 'unlock_button'
+    unlock_button.id = 'unlock_button';
     unlock_button.textContent = 'Unlock';
     unlock_button.addEventListener('click', function() {
         for (e of document.querySelectorAll('[disabled]')) e.disabled = false;
