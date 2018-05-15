d0ca06c81ada4caf48351c416cb52a00629fa09f
ErikBoesen <me@erikboesen.com>
2018-05-15 17:07:43 -0400

    Add debug logs in keyboardcontrol.js

--- keyboardcontrol.js
+++ keyboardcontrol.js
@@ -1,16 +1,19 @@
+console.log('Trying keyboard control at ' + location.href);
+console.log(document.getElementById('previous'));
+console.log(document.getElementById('next'));
+console.log('SummerFast: creating keylistener for keyboard control');
+document.getElementById('next'))
 addEventListener('keydown', function(e) {
-    console.log('SummerFast: creating keylistener for keyboard control');
     var act = document.activeElement;
     if (
         !(act.tagName === 'input' && act.type === 'text')
     ) {
         switch (e.which) {
-            case 80: // P
+            case 37: // Left arrow key
                 document.getElementById('previous').click();
                 break;
-            case 78: // N
-                goNextLesson();
-                window.location='../common/next.html';
+            case 39: // Right arrow key
+                document.getElementById('next').click();
                 break;
         }
     }
